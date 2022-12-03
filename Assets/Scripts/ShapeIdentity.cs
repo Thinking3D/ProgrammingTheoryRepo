96b68e36c8fe56c24a0092317cfed67662268de5
Thinking3D <decasar@gmail.com>
2022-12-03 23:06:30 +0100

    cambio versione unity

---
+++ Assets/Scripts/ShapeIdentity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShapeIdentity : MonoBehaviour
+{
+	public string nome;
+	public string colore;
+	private string frase;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	void ShapeInfo()
+	{
+		
+	}
+	
+	void PrintInfo()
+	{
+		frase = "il mio nome è "+nome+" e sono di colore "+colore;
+	}
+}
---
+++ Assets/Scripts/ShapeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShapeManager : MonoBehaviour
+{
+	public TMP_Text didascalia;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	// OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider.
+	protected void OnMouseDown()
+	{
+		
+	}
+}
